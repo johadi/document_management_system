a56edeca69872736389cbff76a4b06a2a8d5b35a
Shaibu Emmanuel <emmanuel.shaibu@andela.com>
2017-03-30 11:08:08 +0100

    chore(sequelize): setup sequelize with models and migrations

--- server/tests/helpers.js
+++ server/tests/helpers.js
@@ -1,6 +1,6 @@
-import should    from 'should';
+import should from 'should';
 import supertest from 'supertest';
-import server       from '../../app';
+import server from '../../app';
 
 const app = supertest(server);
 
