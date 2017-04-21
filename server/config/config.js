9a15d09df9d84404a40f77396c66b03e57a00409
Shaibu Emmanuel <emmanuel.shaibu@andela.com>
2017-04-21 15:28:36 +0100

    chore(sequelize): fix production url db config

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ const config = {
     logging: false
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres'
   }
 };
